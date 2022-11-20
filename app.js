91c6fae709583fef75e34d9cb842c9b0f93afd35
kdtTetz <xenosign@naver.com>
2022-11-20 09:19:28 +0900

    add

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const express = require('express');
 
 const userRouter = require('./routes/users');
+const boardRouter = require('./routes/board');
 
 const app = express();
 const PORT = 4000;
@@ -12,6 +13,7 @@ app.use(express.static('public'));
 app.use(express.urlencoded({ extended: false }));
 
 app.use('/users', userRouter);
+app.use('/board', boardRouter);
 
 app.get('/', (req, res) => {
   res.render('index');
