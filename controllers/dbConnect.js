4b563bdc6bc91bff316359b5b2870f817f305115
kdtTetz <xenosign@naver.com>
2022-12-04 11:54:11 +0900

    Update dbConnect.js

--- controllers/dbConnect.js
+++ controllers/dbConnect.js
@@ -1,14 +1,14 @@
-// @ts-check
-const mysql = require('mysql');
+// // @ts-check
+// const mysql = require('mysql');
 
-const connection = mysql.createConnection({
-  host: 'localhost',
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  port: 3306,
-  database: process.env.DB_DATABASE,
-});
+// const connection = mysql.createConnection({
+//   host: 'localhost',
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   port: 3306,
+//   database: process.env.DB_DATABASE,
+// });
 
-connection.connect();
+// connection.connect();
 
-module.exports = connection;
+// module.exports = connection;
